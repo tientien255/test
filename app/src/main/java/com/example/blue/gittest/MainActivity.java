5167231ce48130d9ab1b53de30fded86d61205dd
TienThanh <nguyentienthanh255@gmail.com>
2016-03-11 09:29:10 +0700

    four commit

--- app/src/main/java/com/example/blue/gittest/MainActivity.java
+++ app/src/main/java/com/example/blue/gittest/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbara = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbara);
-
     }
 
     @Override
