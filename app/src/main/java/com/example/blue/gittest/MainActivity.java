71bea79806be1fa7ab81bce01f13ddeab301b312
TienThanh <nguyentienthanh255@gmail.com>
2016-03-11 09:41:34 +0700

    six commit

--- app/src/main/java/com/example/blue/gittest/MainActivity.java
+++ app/src/main/java/com/example/blue/gittest/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar t = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(t);
+        Toolbar to = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(to);
     }
 
     @Override
