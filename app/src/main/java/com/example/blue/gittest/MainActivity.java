8b21ecc421c448d56ce76236f0bdaaf65f88d021
TienThanh <nguyentienthanh255@gmail.com>
2016-03-11 09:22:57 +0700

    new fist commit

--- app/src/main/java/com/example/blue/gittest/MainActivity.java
+++ app/src/main/java/com/example/blue/gittest/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbara = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbara);
+        Toolbar toolba = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolba);
 
     }
 
